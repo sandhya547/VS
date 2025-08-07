a8c40916fa3166fe2cbc559338d23090d69fc38b
sandhya547 <113549798+sandhya547@users.noreply.github.com>
2025-08-07 14:47:20 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+function goToCakePage() {
+  window.location.href = "cake.html";
+}
+
+
+function cutCake() {
+  const button = document.getElementById("cutButton");
+  button.disabled = true;
+  button.innerText = "🎉 Cutting...";
+
+celebrate();
+
+
+  const balloonColors = ['#ff4d4d', '#4da6ff', '#33cc33', '#ffff66', '#ff99cc', '#9966ff', '#ff6600'];
+  const container = document.getElementById("balloonContainer");
+
+  for (let i = 0; i < 50; i++) {
+    const balloon = document.createElement("div");
+    balloon.classList.add("balloon");
+    balloon.style.backgroundColor = balloonColors[Math.floor(Math.random() * balloonColors.length)];
+
+    balloon.style.left = Math.random() * 100 + "vw";
+    balloon.style.animationDuration = (4 + Math.random() * 2) + "s";
+    balloon.style.animationDelay = (Math.random() * 0.5) + "s";
+    container.appendChild(balloon);
+  }
+
+  setTimeout(() => {
+    window.location.href = "home.html";
+  }, 6000);
+}
